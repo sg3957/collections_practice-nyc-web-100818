de18f32ed568be338ea8534eb7acf41d0e589aa8
Sydney Grant <sg3957@nyu.edu>
2018-10-09 20:56:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -63,10 +63,6 @@ end
 #   end
 # end
 
-# a = [11,22,31,224,44].each.with_index { |val,index| puts "index: #{index} for #{val}" if val < 30}
-# index: 0 for 11
-# index: 1 for 22
-# => [11, 22, 31, 224, 44]
 
 def add_s(arr)
   arr.collect.each_with_index do |word, index|
